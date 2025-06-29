a43d2b08e4f4eaaefa7a9c7f41a774266780b9f9
samuel asare <samuelaboahasare@gmail.com>
2025-06-29 21:05:24 +0000

    task:4. Full description

--- task_4.sql
+++ task_4.sql
@@ -1,15 +1,17 @@
-USE your_database_name;
+-- task_4.sql
+-- Script to display books table structure without using DESCRIBE/EXPLAIN
 
+USE alx_book_store;
 
 SELECT
-    COLUMN_NAME AS 'Column Name',
-    COLUMN_TYPE AS 'Column Type',
-    IS_NULLABLE AS 'Nullable',
+    COLUMN_NAME AS 'Field',
+    COLUMN_TYPE AS 'Type',
+    IS_NULLABLE AS 'Null',
     COLUMN_KEY AS 'Key',
-    COLUMN_DEFAULT AS 'Default Value',
-    EXTRA AS 'Extra Information'
+    COLUMN_DEFAULT AS 'Default',
+    EXTRA AS 'Extra'
 FROM
     INFORMATION_SCHEMA.COLUMNS
 WHERE
     TABLE_SCHEMA = 'alx_book_store'
-    AND TABLE_NAME ='Books';
+    AND TABLE_NAME = 'books';
