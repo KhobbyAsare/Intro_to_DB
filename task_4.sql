0765917466fd22981d3bdae4dfb7ea23174ad6da
samuel asare <samuelaboahasare@gmail.com>
2025-06-29 20:54:39 +0000

    task:4. Full description

--- task_4.sql
+++ task_4.sql
@@ -1,15 +1,16 @@
 -- task_4.sql
 -- Script to show full description of books table without DESCRIBE/EXPLAIN
 
+USE alx_book_store;
 SELECT
-    COLUMN_NAME,
-    COLUMN_TYPE,
-    IS_NULLABLE,
-    COLUMN_KEY,
-    COLUMN_DEFAULT,
-    EXTRA
+    COLUMN_NAME AS 'Field',
+    COLUMN_TYPE AS 'Type',
+    IS_NULLABLE AS 'Null',
+    COLUMN_KEY AS 'Key',
+    COLUMN_DEFAULT AS 'Default',
+    EXTRA AS 'Extra'
 FROM
     INFORMATION_SCHEMA.COLUMNS
 WHERE
-    TABLE_SCHEMA = DATABASE()
+    TABLE_SCHEMA = 'alx_book_store'
     AND TABLE_NAME = 'books';
